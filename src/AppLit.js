03aee1115054fba1ec01cdee9a8956a6232e817d
luciano.ramello <luciano.ramello@vass.es>
2021-04-08 11:35:55 +0200

    first commit

---
+++ src/AppLit.js
@@ -0,0 +1,20 @@
+import { LitElement, html, css } from 'lit-element';
+
+export class AppLit extends LitElement {
+  static get properties() {
+    return {
+
+    };
+  }
+
+/*   constructor() {
+    super();
+    this.title = 'My app';
+  } */
+
+  render() {
+    return html`
+     
+    `;
+  }
+}
